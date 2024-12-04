0b70d58a54944871bd4a58e29bb538ef203c1f72
Shubham <mohanty.shubham007@gmail.com>
2024-12-04 18:29:17 -0500

    feat: clean up project dir, and add 'add_item' lambda function

---
+++ lambda_functions/add_item/lambda_function.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+import uuid
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Inventory')
+
+def lambda_handler(event, context):
+    # Parse the incoming request body
+    data = json.loads(event['body'])
+    item_id = str(uuid.uuid4())
+    data['id'] = item_id
+
+    # Save the item to DynamoDB
+    table.put_item(Item=data)
+
+    return {
+        'statusCode': 201,
+        'body': json.dumps({'message': 'Item added successfully', 'item': data})
+    }
